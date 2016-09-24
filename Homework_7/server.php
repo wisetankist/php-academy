60a02829687ed025262a2cdea944474e28815d4f
Artem <artemgarbuzov@gmail.com>
2016-09-24 17:17:41 +0300

    Add Homework_7

---
+++ Homework_7/server.php
@@ -0,0 +1,38 @@
+<?php error_reporting( E_ERROR ); ?>
+<?php
+
+$users = array("John" => "user", "Alex" => "admin", "David" => "user", "Jeremy" => "admin");
+
+if ($_POST["name"] || $_POST["role"]) {
+    $name = $_POST["name"];
+    $role = $_POST["role"];
+
+    setUserView($name, $role);
+}
+
+if ($_GET["list"] == "show") {
+    showList($users);
+}
+
+function showList($users)
+{
+    foreach ($users as $key => $value) {
+        if ($value == "admin") {
+            echo "<li><strong>" . $key . "</strong></li>";
+        } else {
+            echo "<li>" . $key . "</li>";
+        }
+    }
+}
+
+function setUserView($name, $role = "")
+{
+    if ($role == "true") {
+        echo $name = "<li><strong>" . $name . "</strong></li>";
+    } else {
+        echo $name = "<li>" . $name . "</li>";
+    }
+}
+
+;
+?>
